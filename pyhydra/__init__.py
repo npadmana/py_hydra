42c9a4adc6bc1e947f81c9d240908f2384db6d03
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2011-10-07 21:01:17 -0400

    Code to read in files, median them etc

--- pyhydra/__init__.py
+++ pyhydra/__init__.py
@@ -1,5 +1,6 @@
 import os.path
 import pysao
+import ph_utils as utils
 
 def ds9(*args, **kwargs):
   return pysao.ds9(path=os.path.expandvars('$DS9'))
--- pyhydra/ph_utils.py
+++ pyhydra/ph_utils.py
@@ -7,23 +7,46 @@ Sep 2011
 import numpy as np
 import pyfits
 
+def load_multiple_fits(flist, hdunum=0, verbose=False, transpose=False, **kwargs):
+  """ Read in all the FITS files specified in flist, 
+  reading in HDU hdnum, and fill a 
+  numpy array with it. 
 
-def median_combine_fits(flist, hdunum=0):
-  """ Median combine the files in flist. 
-
-  flist : list of filenames
-  hdunum : hdus to combine [0, Primary]
-
+  Will raise a ValueError if the arrays don't match in size
   """
   comb = None
   ii = 0
   for fn in flist :
+    if verbose :
+      print "Processing %s now......"%fn
     cc = pyfits.open(fn, memmap=True)
     dat = cc[hdunum].data
+    if transpose :
+      dat = dat.T.copy() 
     if comb is None :
       comb = np.zeros((dat.shape[0], dat.shape[1], len(flist)), dtype='f8')
+      savsize = dat.shape
+    if (dat.shape != savsize) :
+      raise ValueError, 'File %s has an incorrectly shaped array'%fn
     comb[:,:,ii] = dat[:,:]
+    cc.close()
     ii += 1
-  return np.median(comb, axis=2)
+  return comb
+
+
+
+def median_combine(flist=None, arr=None, axis=-1, **kwargs):
+  """ Median combine files.
+
+  flist : list of filenames, use load_multiple_fits
+  axis : -1
+  arr : 
+
+  """
+  if flist is not None :
+    arr = load_multiple_fits(flist, **kwargs)
+  if arr is None :
+    raise SyntaxError, 'Specify either flist or arr'
+  return np.median(arr, axis=axis)
 
 
