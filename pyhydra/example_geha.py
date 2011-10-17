50cc87098a34c4423b65c392123b0c28cd01b24e
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2011-10-17 00:22:22 -0400

    Update the example script

--- pyhydra/example_geha.py
+++ pyhydra/example_geha.py
@@ -10,20 +10,26 @@ cc = pyhydra.HydraRun('test', imfix=pyhydra.utils.transpImage)
 dt = dtype([('x', 'S21'), ('y', 'f8')])
 flist1 = loadtxt(datadir+'bias.plan', dtype=dt)
 flist = [datadir + ff for ff in flist1['x']]
-cc.set_bias_flist(flist)
+cc.set_bias(flist)
 
 
 # Read in 2d flats
 dt = dtype([('x', 'S21'), ('y', 'f8')])
 flist1 = loadtxt(datadir+'flats.plan', dtype=dt)
 flist = [datadir + ff for ff in flist1['x']]
-cc.set_flat2d_flist(flist)
+cc.set_flat2d(flist)
 
 
 # Generate traces
 # The defaults have been tuned for this
 cc.generate_traces()
 
+
+# Load in the arc solutions
+flist1 = loadtxt(datadir+'allarc.plan', dtype=dt)
+ww = flist1['y'] == 20.0
+flist = [datadir + ff['x'] for ff in flist1[ww]]
+cc.set_masterarc(flist, mintrace=20, maxtrace=60)
 
 
 
