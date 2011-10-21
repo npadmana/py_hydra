ed1dabccc4bf819cdc03d520611c4539b0c30ec2
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2011-10-21 18:01:41 -0400

    boxcar cutout functionality

--- pyhydra/ph_utils.py
+++ pyhydra/ph_utils.py
@@ -53,6 +53,39 @@ def transpImage(arr):
   return arr.T.copy()
 
 
+
+def boxcar_cutout(tracelist, img, ivar=None, npix=2):
+  """ Simple boxcar extraction.
+
+  Use pixels [-npix, -npix+1, .., 0, .., npix-1, npix]
+
+  If ivar is set to None, artificially set ivar to 1 for all elements.
+  This is necessary, since the traces might skip a pixel due to roundoff error.
+  """
+  if ivar is None :
+    ivar = img*0.0 + 1.0
+
+  # Define the output arrays and ivars
+  nspec = len(tracelist)
+  nwave = img.shape[1]
+  ntotpix = 2*npix + 1
+  out = np.zeros((nspec, ntotpix, nwave), dtype='f8')
+  outivar = np.zeros((nspec, ntotpix, nwave), dtype='f8')
+
+  # Loop over all the traces
+  for ispec in range(nspec):
+    t1 = tracelist[ispec]
+    pixarr = np.int32(np.round(t1[:, 1]))
+    icen = np.int32(np.round(t1[:,0]))
+    for ii in range(-npix, npix+1) :
+      out[ispec, ii+npix, pixarr] = img[icen + ii, pixarr]
+      outivar[ispec, ii+npix, pixarr] = ivar[icen + ii, pixarr]
+
+
+  return (out, outivar)
+
+
+
 def boxcar_extract(tracelist, img, ivar=None, npix=2):
   """ Simple boxcar extraction.
 
