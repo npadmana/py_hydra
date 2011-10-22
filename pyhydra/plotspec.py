baec634268cefc923c6d095175d8ccd083b418ed
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2011-10-22 06:55:59 -0400

    tweaks to plotting routine

--- pyhydra/plotspec.py
+++ pyhydra/plotspec.py
@@ -3,6 +3,9 @@
 """
 
 import matplotlib.pyplot as plt
+import numpy as np
+
+disp = (7750.11-10372.92)/(4046.13-106.53)
 
 def plotspec(arr):
   ntrace = arr.shape[0]
@@ -12,9 +15,18 @@ def plotspec(arr):
   doLoop = True
   while doLoop :
     plt.clf()
-    plt.plot(arr[ii, :])
 
-    print 'Trace : %02i'%ii
+    # Read out the spectrum, and get some reasonable limits
+    spec = arr[ii, :]
+    wave = np.arange(spec.size)
+    wave = (wave-106.53) * disp + 10372.92
+    med = np.median(spec)
+    mad = np.median(np.absolute(spec - med))
+
+    plt.plot(wave,spec)
+    plt.ylim(med-10*mad, med+10*mad)
+
+    print 'Trace : %02i, MAD=%f'%(ii, mad)
     
     sel = raw_input()
     if sel == 'q' :
