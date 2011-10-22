eff3de37ce62c0780bd9dea209d6186742a24194
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2011-10-22 06:57:02 -0400

    continued hackery of this script

--- pyhydra/redux_Oct2011/process_all.py
+++ pyhydra/redux_Oct2011/process_all.py
@@ -11,14 +11,18 @@ cc = load_hydrarun('test', 'Trace1')
 
 datadir = '../Oct21/'
 outdir = 'Oct21/'
-flist3 = ['field-DEEP2-23hr-1%02i.fits'%ii for ii in range(1)]
-skylist = ['sky-DEEP2-23hr-1%02i.fits'%ii for ii in range(1)]
+flist3 = ['field-DEEP2-23hr-1%02i.fits'%ii for ii in range(7)]
+skylist = ['sky-DEEP2-23hr-1%02i.fits'%ii for ii in range(2)]
+flist4 = ['field-EBOSS%02i.fits'%ii for ii in range(5)]
+skylist2 = ['sky-EBOSS%02i.fits'%ii for ii in range(1)]
 
 flist = []
 #flist.extend(flist1)
 #flist.extend(flist2)
 flist.extend(flist3)
 flist.extend(skylist)
+flist.extend(flist4)
+flist.extend(skylist2)
 
 
 # Process all the files in flist
