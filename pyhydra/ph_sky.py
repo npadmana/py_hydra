a0ce34c992637449dfdfa98662d9acbefb07fccc
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2011-10-24 21:10:26 -0400

    Work on improving the sky subtraction

--- pyhydra/ph_sky.py
+++ pyhydra/ph_sky.py
@@ -103,7 +103,7 @@ class PCASkySubtract :
 
   def reset_optimizer(self, nparam):
     self.nparam = nparam
-    self.opt = nlopt.opt(nlopt.LN_COBYLA, nparam)
+    self.opt = nlopt.opt(nlopt.LD_TNEWTON, nparam)
     self.opt.set_ftol_rel(1.e-6)
     self.opt.set_xtol_rel(1.e-8)
     self.opt.set_ftol_abs(1.e-6)
@@ -120,6 +120,12 @@ class PCASkySubtract :
   def minfunc(self, vec, grad):
     rr = self.resid(vec)
     chi2 = rr**2 * self.ivar
+    if grad is not None :
+      if len(grad) > 0 :
+        for ii in range(self.nparam):
+          tmp = (2.0*rr*self.eigenspec[ii, :] * self.ivar)
+          grad[ii] = -tmp[self.startpix:self.endpix].sum()
+
     return (chi2[self.startpix:self.endpix]).sum()
   
 
@@ -135,6 +141,75 @@ class PCASkySubtract :
 
     return (rr, outvec, chi2)
 
+
+
+class DerivSkySubtract :
+  def __init__(self, skyspec, startpix=500, endpix=4000):
+    self.sky0 = skyspec
+    skyp1 = ndimage.shift(skyspec, 0.1)
+    self.dsky = (skyp1 - skyspec)/0.1
+
+    self.data = None
+    self.ivar = None
+    self.startpix = startpix
+    self.endpix = endpix
+    npix = len(skyspec)
+    self.xpix = np.arange(npix, dtype='f8')/npix
+    self.nparam = 2  + 3
+
+    self.opt = nlopt.opt(nlopt.LD_TNEWTON, self.nparam)
+    self.opt.set_ftol_rel(1.e-6)
+    self.opt.set_xtol_rel(1.e-8)
+    self.opt.set_ftol_abs(1.e-6)
+    self.opt.set_xtol_abs(1.e-8)
+    self.opt.set_min_objective(self.minfunc)
+
+  def resid(self, vec):
+    pp = np.poly1d(vec[1:])
+    model = vec[0] * self.sky0 + (pp(self.xpix))*self.dsky 
+    return self.data - model
+
+
+  def minfunc(self, vec, grad):
+    self.curvec = vec
+    rr = self.resid(vec)
+    chi2 = rr**2 * self.ivar
+    if grad is not None :
+      if  len(grad) > 0 :
+        tmp = (2.0*rr*self.sky0 * self.ivar)
+        grad[0] = -tmp[self.startpix:self.endpix].sum()
+        # Not pretty, and can be done much better but...
+        for ii in range(1,self.nparam):
+          tmp = 2.0*rr*self.dsky*self.ivar*self.xpix**(self.nparam-ii-1)
+          grad[ii] = -tmp[self.startpix:self.endpix].sum()
+    return (chi2[self.startpix:self.endpix]).sum()
+  
+  def check_diff(self, vec, dx=1.e-1):
+    grad = 0.0*vec
+    out1 = self.minfunc(vec, grad)
+    print grad
+
+    grad1 = 0.0*grad
+    for ii in range(vec.size):
+      vec1 = vec*1.0
+      vec1[ii] += dx
+      y1 = self.minfunc(vec1,  grad)
+      vec1[ii] -= 2*dx
+      y2 = self.minfunc(vec1, grad)
+      grad1[ii] = (y1-y2)/(2*dx)
+
+    print grad1
+
+  def skysubtract(self, data, ivar):
+    self.data = data
+    self.ivar = ivar
+
+    startvec = np.zeros((self.nparam, ), dtype='f8')
+    outvec = self.opt.optimize(startvec)
+    rr = self.resid(outvec)
+    chi2 = self.minfunc(outvec, None)
+
+    return (rr, outvec, chi2)
 
 
 
